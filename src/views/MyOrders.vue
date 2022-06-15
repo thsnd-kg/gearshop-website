69c2e9092831f1697b3aa3df991dc01c5dce9715
Phạm Tấn khang <phamtankhang2001@gmail.com>
2022-06-15 11:01:13 +0700

    handle redirect page

--- src/views/MyOrders.vue
+++ src/views/MyOrders.vue
@@ -1,30 +1,27 @@
 <template>
-    <div class="wrapper">
-        <v-breadcrumbs :items="breadcrumb">
+  <div class="wrapper">
+    <v-breadcrumbs :items="breadcrumb">
       <template v-slot:divider>
         <v-icon>mdi-chevron-right</v-icon>
       </template>
     </v-breadcrumbs>
     <v-row>
-        <v-col 
-        cols="8" 
-        v-for="data in items"
-        :key="data.orderId"
-        >
-         <OrderItem :items="data"/>
-        </v-col>
+      <v-col cols="8" v-for="data in items" :key="data.orderId">
+        <OrderItem :items="data" />
+      </v-col>
     </v-row>
-    </div>
+  </div>
 </template>
 <script>
-import OrderItem from "../components/OrderItem.vue"
+import OrderItem from '../components/OrderItem.vue';
 export default {
-      components: {
-    OrderItem
+  components: {
+    OrderItem,
   },
+
   data() {
     return {
-         breadcrumb:[
+      breadcrumb: [
         {
           text: 'Trang chủ',
           disabled: false,
@@ -36,30 +33,29 @@ export default {
           href: '',
         },
       ],
-         items: {},
-    }
+      items: {},
+    };
   },
   methods: {
- async getOrders() {
-const response = await this.$http.get(`orders/user`);
-      if(response.status == 200) {
-        this.items = response.content
+    async getOrders() {
+      const response = await this.$http.get(`orders/user`);
+      if (response.status == 200) {
+        this.items = response.content;
+      } else {
+        this.$notify.warning('Bạn chưa có đơn hàng nào cả!');
       }
-      else {
-        this.$notify.warning("Bạn chưa có đơn hàng nào cả!")
-      }
- }
+    },
   },
   created() {
- this.getOrders()
-  }
-}
+    this.getOrders();
+  },
+};
 </script>
 <style lang="scss" scoped>
 .wrapper {
   margin: auto;
   width: 1100px;
-  .title{
+  .title {
     margin-bottom: 10px;
   }
 }
